6795e7cc89f23f007f798fe269b4e2ec0e67cd65
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-08-25 13:15:20 +0600

    profile route bug fixed

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,3 @@
-const { route } = require("../app");
 const { authenticateUser } = require("../service/authMiddleWares");
 
 const router = require("express-promise-router")();
@@ -6,7 +5,7 @@ const ProfileController=require('../controllers/profile').ProfileController
 
 const profileController=new ProfileController()
 
-router.route("/getUserId").get(authenticateUser,profileController.get);
+router.route("/get").get(authenticateUser,profileController.get);
 router.route("/update").put(authenticateUser,profileController.update);
 
 module.exports=router
